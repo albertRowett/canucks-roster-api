116c3a24d0e1a90c400a3f5527c1631d8d9c80db
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2024-05-08 09:49:43 +0200

    Create tests for GET requests to /players/{jerseyNumber} endpoint to protect format of JSON responses

--- tests/Feature/PlayerTest.php
+++ tests/Feature/PlayerTest.php
@@ -34,4 +34,36 @@ class PlayerTest extends TestCase
                     ->where('message', 'Players successfully retrieved');
             });
     }
+
+    public function test_get_player_by_jersey_number_success(): void
+    {
+        $player = Player::factory()->has(PreviousTeam::factory())->create();
+
+        $response = $this->getJson("/api/players/$player->jersey_number");
+        $response->assertStatus(200)
+            ->assertJson(function (AssertableJson $json) {
+                $json->has('data', 1, function (AssertableJson $json) {
+                    $json->whereAllType([
+                        'id' => 'integer',
+                        'name' => 'string',
+                        'jersey_number' => 'integer',
+                        'date_of_birth' => 'string',
+                        'position.name' => 'string',
+                        'nationality.name' => 'string',
+                        'draft_team.team.name' => 'string',
+                        'previous_teams.0.team.name' => 'string',
+                    ]);
+                })
+                    ->where('message', 'Player successfully retrieved');
+            });
+    }
+
+    public function test_player_not_found(): void
+    {
+        $response = $this->getJson('/api/players/1');
+        $response->assertStatus(404)
+            ->assertJson(function (AssertableJson $json) {
+                $json->where('message', 'Player with jersey number 1 not found');
+            });
+    }
 }
