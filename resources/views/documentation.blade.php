8beed56f79876aab53fe4e5fe28d8a2f06a53c0f
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2024-07-08 13:57:49 +0200

    Create documentation view and assign index route to return this

---
+++ resources/views/documentation.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Vancouver Canucks Roster API: Documentation</title>
+
+    <meta name="description" content="" />
+    <meta name="author" content="Albert Rowett" />
+</head>
+
+<body>
+
+</body>
+
+</html>
