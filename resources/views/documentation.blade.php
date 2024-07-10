07af20fb5534aea237511722d05427319c5e96d3
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2024-07-10 14:44:22 +0200

    Docs: apply CSS up to end of first section ('Return all players' API call)

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -9,121 +9,117 @@
 
     <meta name="description" content="" />
     <meta name="author" content="Albert Rowett" />
+
+    <link rel="stylesheet" href="css/documentation.css" />
 </head>
 
 <body>
-    <h1>Canucks Roster API</h1>
-    <h2>API Documentation</h2>
-    <h3>Return all players - optionally filtered</h3>
-    <ul>
-        <li>
-            <b>URL:</b><br>
-            /api/players
-        </li>
-        <li>
-            <b>Method:</b><br>
-            <code>GET</code>
-        </li>
-        <li>
-            <b>URL Params:</b><br>
-            <b>Required:</b><br>
-            There are no required URL params<br>
-            <b>Optional:</b><br>
-            <ul>
-                <li><code>removed=0|1</code> - 1 to return only players removed from roster (if unspecified, only active players returned, i.e. <code>removed=0</code>)</li>
-                <li><code>position=string</code> - Filter results by position name</li>
-                <li><code>nationality=string</code> - Filter results by nationality name</li>
-            </ul>
-            <b>Example:</b><br>
-            <code>/api/players?removed=1&position=Center&nationality=USA</code>
-        </li>
-        <li>
-            <b>Success Response:</b><br>
-            <ul>
-                <li><b>Code:</b> 200</li><br>
-                <b>Content:</b>
-                <pre>
-                    <code>
-{
-    "data": [
+    <div class="container">
+        <h1>Vancouver Canucks Roster API</h1>
+        <h2>API Documentation</h2>
+        <h3>Return all players - optionally filtered</h3>
+        <ul>
+            <li>
+                <p class="bold">URL:</p>
+                <p>/api/players</p>
+            </li>
+            <li>
+                <p class="bold">Method:</p>
+                <p><code class="code">GET</code></p>
+            </li>
+            <li>
+                <p class="bold">URL Params:</p>
+                <p class="bold">Required:</p>
+                <p>There are no required URL params</p>
+                <p class="bold">Optional:</p>
+                <ul>
+                    <li><code class="code">removed=0|1</code> - 1 to return only players removed from roster (if unspecified, only active players returned, i.e. <code class="code">removed=0</code>)</li>
+                    <li class="subsequent-bullet"><code class="code">position=string</code> - Filter results by position name</li>
+                    <li class="subsequent-bullet"><code class="code">nationality=string</code> - Filter results by nationality name</li>
+                </ul>
+                <p class="bold">Example:</p>
+                <p><code class="code">/api/players?removed=1&position=Center&nationality=USA</code>
+                <p>
+            </li>
+            <li>
+                <p class="bold">Success Response:</p>
+                <ul>
+                    <li>
+                        <p><span class="bold">Code:</span> 200</p>
+                    </li>
+                    <p class="bold">Content:</p>
+                    <pre class="code codeblock"><code>{
+    <span class="json-key">"data"</span>: [
         {
-            "id": 1,
-            "name": "J. T. Miller",
-            "jersey_number": 9,
-            "date_of_birth": "1993-03-14",
-            "position": {
-                "name": "Center"
+            <span class="json-key">"id"</span>: <span class="json-int">1</span>,
+            <span class="json-key">"name"</span>: <span class="json-str">"J. T. Miller"</span>,
+            <span class="json-key">"jersey_number"</span>: <span class="json-int">9</span>,
+            <span class="json-key">"date_of_birth"</span>: <span class="json-str">"1993-03-14"</span>,
+            <span class="json-key">"position"</span>: {
+                <span class="json-key">"name"</span>: <span class="json-str">"Center"</span>
             },
-            "draft_team": {
-                "team": {
-                    "name": "New York Rangers"
+            <span class="json-key">"nationality"</span> {
+                <span class="json-key">"name"</span>: <span class="json-str">"USA"</span>
+            },
+            <span class="json-key">"draft_team"</span>: {
+                <span class="json-key">"team"</span>: {
+                    <span class="json-key">"name"</span>: <span class="json-str">"New York Rangers"</span>
                 }
             },
-            "previous_teams": [
+            <span class="json-key">"previous_teams"</span>: [
                 {
-                    "team": {
-                        "name": "New York Rangers"
+                    <span class="json-key">"team"</span>: {
+                        <span class="json-key">"name"</span>: <span class="json-str">"New York Rangers"</span>
                     }
                 },
                 {
-                    "team": {
-                        "name": "Tampa Bay Lightning"
+                    <span class="json-key">"team"</span>: {
+                        <span class="json-key">"name"</span>: <span class="json-str">"Tampa Bay Lightning"</span>
                     }
                 }
             ]
         }
     ],
-    "message": "Players successfully retrieved"
-}
-                    </code>
-                </pre>
-            </ul>
-        </li>
-        <li>
-            <b>Error Response:</b><br>
-            <ul>
-                <li><b>Code:</b> 422 UNPROCESSABLE CONTENT</li><br>
-                <b>Content:</b>
-                <pre>
-                    <code>
-{
-    "message": "The removed field must be true or false. (and 2 more errors)",
-    "errors": {
-        "removed": [
-            "The removed field must be true or false."
-        ],
-        "position": [
-            "The selected position is invalid."
-        ],
-        "nationality": [
-            "The selected nationality is invalid."
-        ]
+    <span class="json-key">"message"</span>: <span class="json-str">"Players successfully retrieved"</span>
+}</code></pre>
+                </ul>
+            </li>
+            <li>
+                <p class="bold">Error Response:</p>
+                <ul>
+                    <li>
+                        <p><span class="bold">Code:</span> 422 UNPROCESSABLE CONTENT</p>
+                    </li>
+                    <p class="bold">Content:</p>
+                    <pre class="code codeblock"><code>{
+    <span class="json-key">"message"</span>: <span class="json-str">"The removed field must be true or false. (and 2 more errors)"</span>,
+    <span class="json-key">"errors"</span>: {
+        <span class="json-key">"removed"</span>: [<span class="json-str">"The removed field must be true or false."</span>],
+        <span class="json-key">"position"</span>: [<span class="json-str">"The selected position is invalid."</span>],
+        <span class="json-key">"nationality"</span>: [<span class="json-str">"The selected nationality is invalid."</span>]
     }
-}
-                    </code>
-                </pre>
-            </ul>
-        </li>
-        <li>
-            <b>Sample Call:</b><br>
-            <ul>
-                <li><b>JavaScript (<code>`fetch`</code>):</b></li>
-            </ul>
-            <pre>
-                <code>                
-fetch('/api/players?removed=1&position=Center&nationality=USA', {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json'
+}</code></pre>
+                </ul>
+            </li>
+            <li>
+                <p class="bold">Sample Call:</p>
+                <ul>
+                    <li>
+                        <p><span class="bold">JavaScript</span> (<code>`fetch`</code>):</p>
+                    </li>
+                    <pre class="code codeblock"><code><span class="js-method">fetch</span>(<span class="js-argument">'/api/players?removed=1&position=Center&nationality=USA'</span>, {
+    <span class="js-property-key">method</span>: <span class="js-property-value">'GET'</span>,
+    <span class="js-property-key">headers</span>: {
+        <span class="js-property-value">'Accept'</span>: <span class="js-property-value">'application/json'</span>
     },
-    mode: 'cors'
+    <span class="js-property-key">mode</span>: <span class="js-property-value">'cors'</span>
 })
-.then(response => response.json())
-.then(data => console.log(data));
-                </code>
-            </pre>
-        </li>
-    </ul>
+    .<span class="js-method">then</span>(response <span class="js-arrow">=></span> response.<span class="js-method">json</span>())
+    .<span class="js-method">then</span>(data <span class="js-arrow">=></span> console.<span class="js-method">log</span>(data));</code></pre>
+                </ul>
+            </li>
+        </ul>
+    </div>
 </body>
 
 </html>
